fc4cfbb91f38597b47ee6c15ae2f06df9e5b53e2
Cameron <camcreel@gmail.com>
2021-05-20 20:49:32 -0400

    Completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,88 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+
+function ShoppingList({ items,setItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
+  
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    
+    setSearch(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item) => {
+
+    if (selectedCategory === "All") {
+
+      if (search){
+        
+        return item.name.includes(search);
+      }
+      else {
+        return true;
+      }
+
+    } else {
+
+      if (search){
+        return item.category === selectedCategory &&  item.name.includes(search);
+      }
+      else {
+        
+        return item.category === selectedCategory;
+      }
+      
+    }
+    
+    
   });
 
+
+
+  // const [itemName,setName] = useState("")
+  // const [formCategory,setFormCategory] = useState("Produce")
+
+  // function handleFormCategory(e) {
+  //   const newCategory = e.target.value;
+    
+  //   setFormCategory(newCategory)
+  // }
+
+  // function handleNameChange(e) {
+    
+  //   const newName = e.target.value;
+  //   setName(newName)
+  // }
+
+
+  // function handleItemFormSubmit(newItem) {
+  //   console.log(newItem)
+    
+  //   setItems([...items,newItem])
+  //   setName("")
+  // }
+
+  function handleItemFormSubmit(newItem) {
+    
+    setItems([...items,newItem])
+    
+  }
+
+ 
+
+ 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm 
+        onItemFormSubmit={handleItemFormSubmit}  
+      />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search= {search}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
